f2e8c8302941e32aedae656d313de1c7e45120af
Daniel <danielcondor9@gmail.com>
2021-01-22 17:00:41 -0500

    servicios de combo

--- src/app/services/api/combo/combo.service.ts
+++ src/app/services/api/combo/combo.service.ts
@@ -13,6 +13,79 @@ export class ComboService {
     private _http: HttpClient,
     private _appUsuarioService: AppUsuarioService
   ) {}
+  obtenerComboCategoriaICC(): Observable<any> {
+    const url = `${environment.baseUri}${ApiPaths.obtenerComboCategoriaICC}`;
+    const token = this._appUsuarioService.obtenerToken();
+    const tipoToken = this._appUsuarioService.obtenerTipoToken();
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `${tipoToken} ${token}`,
+    });
+
+    return this._http.get(url, {
+      headers: headers,
+    });
+  }
+
+  obtenerComboCategoriaIMC(): Observable<any> {
+    const url = `${environment.baseUri}${ApiPaths.obtenerComboCategoriaIMC}`;
+    const token = this._appUsuarioService.obtenerToken();
+    const tipoToken = this._appUsuarioService.obtenerTipoToken();
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `${tipoToken} ${token}`,
+    });
+
+    return this._http.get(url, {
+      headers: headers,
+    });
+  }
+  obtenerComboRol(): Observable<any> {
+    const url = `${environment.baseUri}${ApiPaths.obtenerComboRol}`;
+    const token = this._appUsuarioService.obtenerToken();
+    const tipoToken = this._appUsuarioService.obtenerTipoToken();
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `${tipoToken} ${token}`,
+    });
+
+    return this._http.get(url, {
+      headers: headers,
+    });
+  }
+
+  obtenerComboSexo(): Observable<any> {
+    const url = `${environment.baseUri}${ApiPaths.obtenerComboSexo}`;
+    const token = this._appUsuarioService.obtenerToken();
+    const tipoToken = this._appUsuarioService.obtenerTipoToken();
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `${tipoToken} ${token}`,
+    });
+
+    return this._http.get(url, {
+      headers: headers,
+    });
+  }
+
+  obtenerComboNivelEstres(): Observable<any> {
+    const url = `${environment.baseUri}${ApiPaths.obtenerComboNivelEstres}`;
+    const token = this._appUsuarioService.obtenerToken();
+    const tipoToken = this._appUsuarioService.obtenerTipoToken();
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `${tipoToken} ${token}`,
+    });
+
+    return this._http.get(url, {
+      headers: headers,
+    });
+  }
 
   obtenerComboRendimientoAcademico(): Observable<any> {
     const url = `${environment.baseUri}${ApiPaths.obtenerComboRendimientoAcademico}`;
