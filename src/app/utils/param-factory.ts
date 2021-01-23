98e4ac5b5f511f8234ce996292c8ef95d990c1ef
Ignacio <ignacioruedaboada@hotmail.com>
2021-01-23 01:29:02 -0500

    Corregido param factory build

--- src/app/utils/param-factory.ts
+++ src/app/utils/param-factory.ts
@@ -2,12 +2,11 @@ import { HttpParams } from "@angular/common/http";
 
 export class ParamFactory {
     static build<T>(data: T): HttpParams {
-        const params = new HttpParams();
+        let params = new HttpParams();
 
         Object.entries(data).forEach(item => {
-            params.append(item[0], item[1]);
+            params = params.append(item[0], item[1]);
         });
-
         return params;
     }
 }
