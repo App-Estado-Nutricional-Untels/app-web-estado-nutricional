ff1ca08b023d0f6ebeb6a4197cccc2c2b26b66b0
Daniel <danielcondor9@gmail.com>
2021-01-25 11:12:09 -0500

    3 graficas del admin---- merge

--- src/app/components/view/registro/registro.component.ts
+++ src/app/components/view/registro/registro.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { RegistroBody } from 'src/app/models/request/body/registro-body.interface';
+
+enum RegistroEstado {
+  INICIAL,
+  CARGANDO
+}
 
 declare const M: any;
 
@@ -10,20 +17,43 @@ declare const M: any;
 })
 export class RegistroComponent implements OnInit {
 
+  public Estados = RegistroEstado;
+
   parteRegistro: number = 1;
 
-  constructor(private _toastr: ToastrService) { }
+  estado: RegistroEstado = RegistroEstado.INICIAL;
+
+  formulario: RegistroBody = {
+    nombres: '',
+    apePaterno: '',
+    apeMaterno: '',
+    correoElectronico: '',
+    dni: '',
+    sexo: '',
+    rol: '',
+    codigo: '',
+    clave: ''
+  };
+
+  constructor(
+    private _router: Router,
+    private _toastr: ToastrService) { }
 
   siguienteParte(parte: number): void {
     this.parteRegistro = parte;
   }
-
+  selecionarRol(rol:string):void{
+    this.formulario.rol= rol;
+  }
   registrar(event: Event): void {
     event.preventDefault();
+    this.estado = RegistroEstado.CARGANDO;
     this._toastr.error('Ocurrió un error');
+    console.log(this.formulario);
   }
 
   ngOnInit(): void {
+    console.log()
   }
 
   ngAfterViewInit(): void {
