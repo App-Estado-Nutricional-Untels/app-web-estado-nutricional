9b102c3e3d8e92e8f3dc37e3c052a8d8eed717f5
Daniel <danielcondor9@gmail.com>
2021-01-25 20:10:01 -0500

    graficas grupales en busqueda de admin

--- src/app/components/common/informe-grupal-imc/informe-imc-grupal-porcentaje-nivel-estres-card/informe-imc-grupal-porcentaje-nivel-estres-card.component.ts
+++ src/app/components/common/informe-grupal-imc/informe-imc-grupal-porcentaje-nivel-estres-card/informe-imc-grupal-porcentaje-nivel-estres-card.component.ts
@@ -1,5 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit,SimpleChanges } from '@angular/core';
+import { ChartOptions, ChartType } from 'chart.js';
+import { Label, SingleDataSet } from 'ng2-charts';
 import { ReporteEstres } from 'src/app/models/reporte-imc/reporte-estres.interface';
+
+enum InformeImcGrupalPorcentajeNivelEstresCardEstados {
+  VACIO,
+  CARGANDO,
+  CON_DATOS
+}
 
 @Component({
   selector: 'app-informe-imc-grupal-porcentaje-nivel-estres-card',
@@ -10,9 +18,50 @@ export class InformeImcGrupalPorcentajeNivelEstresCardComponent implements OnIni
 
   @Input() reporteEstres: ReporteEstres | undefined;
 
+  public Estados = InformeImcGrupalPorcentajeNivelEstresCardEstados;
+
+  public estado: InformeImcGrupalPorcentajeNivelEstresCardEstados 
+    = InformeImcGrupalPorcentajeNivelEstresCardEstados.VACIO;
+
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+  };
+
+  public pieChartLabels: Label[] = [];
+  public pieChartData: SingleDataSet = [];
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
+
+  public colores = [{
+    backgroundColor:[
+      'rgba(134, 199, 243, 1)',
+      'rgba(255, 161, 181, 1)', 
+      'rgba(180, 169, 20, 1)', 
+      //OJO: Si hay mas de 2 datos, colocar mas colores
+    ]
+  }];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.estado = InformeImcGrupalPorcentajeNivelEstresCardEstados.CARGANDO;
+    if (this.reporteEstres) {
+      this.pieChartData.push(this.reporteEstres.Severo.porcentaje);
+      this.pieChartData.push(this.reporteEstres.Moderado.porcentaje);
+      this.pieChartData.push(this.reporteEstres.Leve.porcentaje);
+
+      this.pieChartLabels.push('Severo');
+      this.pieChartLabels.push('Moderado');
+      this.pieChartLabels.push('Leve');
+      this.estado = InformeImcGrupalPorcentajeNivelEstresCardEstados.CON_DATOS;
+      console.log('aja');
+    } else {
+      this.estado = InformeImcGrupalPorcentajeNivelEstresCardEstados.VACIO;
+    }
   }
 
 }
--- src/app/components/common/informe-grupal-imc/informe-imc-grupal-porcentaje-rendimiento-academico-card/informe-imc-grupal-porcentaje-rendimiento-academico-card.component.ts
+++ src/app/components/common/informe-grupal-imc/informe-imc-grupal-porcentaje-rendimiento-academico-card/informe-imc-grupal-porcentaje-rendimiento-academico-card.component.ts
@@ -1,6 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit,SimpleChanges } from '@angular/core';
+import { ChartOptions, ChartType } from 'chart.js';
+import { Label, SingleDataSet } from 'ng2-charts';
 import { ReporteRendimientoAcademico } from 'src/app/models/reporte-imc/reporte-rendimiento-academico.interface';
-
+enum InformeImcGrupalPorcentajeRendimientoAcademicoCardEstados {
+  VACIO,
+  CARGANDO,
+  CON_DATOS
+}
 @Component({
   selector: 'app-informe-imc-grupal-porcentaje-rendimiento-academico-card',
   templateUrl: './informe-imc-grupal-porcentaje-rendimiento-academico-card.component.html',
@@ -10,9 +16,57 @@ export class InformeImcGrupalPorcentajeRendimientoAcademicoCardComponent impleme
 
   @Input() reporteRendimientoAcademico: ReporteRendimientoAcademico | undefined;
   
+  public Estados = InformeImcGrupalPorcentajeRendimientoAcademicoCardEstados;
+
+  public estado: InformeImcGrupalPorcentajeRendimientoAcademicoCardEstados 
+    = InformeImcGrupalPorcentajeRendimientoAcademicoCardEstados.VACIO;
+
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+  };
+
+  public pieChartLabels: Label[] = [];
+  public pieChartData: SingleDataSet = [];
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
+
+  public colores = [{
+    backgroundColor:[
+      'rgba(134, 199, 243, 1)',
+      'rgba(255, 161, 181, 1)', 
+      'rgba(18, 199, 243, 1)',
+      'rgba(22, 161, 181, 1)', 
+      'rgba(122, 190, 12, 1)',
+      //OJO: Si hay mas de 2 datos, colocar mas colores
+    ]
+  }];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.estado = InformeImcGrupalPorcentajeRendimientoAcademicoCardEstados.CARGANDO;
+    if (this.reporteRendimientoAcademico) {
+      this.pieChartData.push(this.reporteRendimientoAcademico.Bueno.porcentaje);
+      this.pieChartData.push(this.reporteRendimientoAcademico.Excelente.porcentaje);
+      this.pieChartData.push(this.reporteRendimientoAcademico.Malo.porcentaje);
+      this.pieChartData.push(this.reporteRendimientoAcademico.Normal.porcentaje);
+      this.pieChartData.push(this.reporteRendimientoAcademico.Pesimo.porcentaje);
+
+      this.pieChartLabels.push('Bueno');
+      this.pieChartLabels.push('Excelente');
+      this.pieChartLabels.push('Malo');
+      this.pieChartLabels.push('Normal');
+      this.pieChartLabels.push('Pesimo');
+
+      this.estado = InformeImcGrupalPorcentajeRendimientoAcademicoCardEstados.CON_DATOS;
+
+      console.log('aja');
+    } else {
+      this.estado = InformeImcGrupalPorcentajeRendimientoAcademicoCardEstados.VACIO;
+    }
   }
 
 }
