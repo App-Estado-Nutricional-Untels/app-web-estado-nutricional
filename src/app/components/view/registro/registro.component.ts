bb5abc754d7d19d02657526faa6bb71a6de53297
Ignacio <ignacioruedaboada@hotmail.com>
2021-01-26 21:08:36 -0500

    Registro

--- src/app/components/view/registro/registro.component.ts
+++ src/app/components/view/registro/registro.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { RegistroBody } from 'src/app/models/request/body/registro-body.interface';
+import { AutenticacionService } from 'src/app/services/api/autenticacion/autenticacion.service';
+import { ApiErrorResponseMessageFactory } from 'src/app/utils/api-error-response-message-factory';
 
 enum RegistroEstado {
   INICIAL,
@@ -35,19 +37,37 @@ export class RegistroComponent implements OnInit {
 
   constructor(
     private _router: Router,
-    private _toastr: ToastrService) { }
+    private _autenticacionService: AutenticacionService,
+    private _toastr: ToastrService,
+  ) { }
 
-  siguienteParte(parte: number): void {
+  mostrarParte(parte: number): void {
     this.parteRegistro = parte;
   }
+
   selecionarRol(rol:string):void{
     this.formulario.rol= rol;
   }
+
   registrar(event: Event): void {
     event.preventDefault();
     this.estado = RegistroEstado.CARGANDO;
-    this._toastr.error('Ocurrió un error');
-    console.log(this.formulario);
+    this._autenticacionService
+      .registro(this.formulario)
+      .subscribe(
+        (resp) => {
+          this._toastr.success(resp.mensaje);
+          this.estado = RegistroEstado.INICIAL;
+          setTimeout(() => this._router.navigate(
+            ["/iniciar-sesion"], {replaceUrl: true}), 2000);
+        },
+        (respError) => {
+          console.warn(respError);
+          const msg = ApiErrorResponseMessageFactory.build(respError.error);
+          this._toastr.error(msg);
+          this.estado = RegistroEstado.INICIAL;
+        }
+      );
   }
 
   ngOnInit(): void {
